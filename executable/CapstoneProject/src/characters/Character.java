421b88d7f0ca7e5e4907fa50a509575006571aa8
HoodieIshaan <imusunuri577@student.fuhsd.org>
2022-05-10 01:01:21 -0700

    Update Character.java

--- executable/CapstoneProject/src/characters/Character.java
+++ executable/CapstoneProject/src/characters/Character.java
@@ -1,6 +1,28 @@
 package characters;
 
-public class Character
+public abstract class Character
 {
+	private double health;
+	private double damage;
+	private int attackPow;
+	private int defensePow;
+	
+	public void doDamage() {
+		
+	}
+	 public double getHealth()
+	 {
+		 return health;
+	 }
+	 public double getDamage()
+	 {
+		 return health;
+	 }
+	public int getAttackPow() {
+		return attackPow;
+	}
+	public int getDefensePow() {
+		return defensePow;
+	}
 	
 }
