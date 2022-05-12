c2e972f3a6ad4d41a3a15080aea27784dd60e084
HoodieIshaan <imusunuri577@student.fuhsd.org>
2022-05-11 18:57:23 -0700

    Alpha Release

--- executable/CapstoneProject/src/screens/Menu.java
+++ executable/CapstoneProject/src/screens/Menu.java
@@ -1,5 +1,10 @@
 package screens;
-public class Menu
+public class Menu extends Screen
 {
+
+	public Menu(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
 	
 }
