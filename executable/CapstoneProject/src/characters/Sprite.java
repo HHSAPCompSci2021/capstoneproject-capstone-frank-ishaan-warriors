ba120a64d27dcdd0a74404fc9134c5255bdd6a5e
HoodieIshaan <imusunuri577@student.fuhsd.org>
2022-05-17 14:13:54 -0700

    Update Sprite.java

--- executable/CapstoneProject/src/characters/Sprite.java
+++ executable/CapstoneProject/src/characters/Sprite.java
@@ -7,77 +7,7 @@ import processing.core.PImage;
 
 public class Sprite extends Rectangle2D.Double
 {
-	/*private double health;
-	private double damage;
-	private int attackPow;
-	private int defensePow;
 	
-	
-	// FIELDS
-		private PImage image;
-		
-		// CONSTRUCTORS
-		
-		public Sprite(int x, int y, int w, int h) {
-			this(null, x, y, w, h);
-		}
-		
-		public Sprite(PImage img, int x, int y, int w, int h) {
-			super(x,y,w,h);
-			image = img;
-		}
-		
-	public Sprite(double health, double damage, int defensePow, int attackPow)
-	{
-		this.health = health;
-		this.attackPow = attackPow;
-		this.defensePow = defensePow;
-		this.damage = damage;
-	}
-	
-	public void doDamage() {
-		damage -= 5;
-	}
-	/** 
-	 * 
-	 * @return health of the sprite
-	 *//*
-	 public double getHealth()
-	 {
-		 return health;
-	 }
-	 /** 
-		 * 
-		 * @return damage of the sprite
-		 *//*
-	 public double getDamage()
-	 {
-		 return health;
-	 }
-	 /** 
-		 * 
-		 * @return attack power of the sprite
-		 *//*
-	public int getAttackPow() {
-		return attackPow;
-	}
-	/** 
-	 * 
-	 * @return defense power of the sprite
-	 *//*
-	public int getDefensePow() {
-		return defensePow;
-	}
-	
-	public void moveX(int moveBy)
-	{
-		
-	}
-	
-	public void moveY(int moveBy)
-	{
-		
-	}*/
 	private PImage image;
 	protected int health;
 	protected String specialImgName;
